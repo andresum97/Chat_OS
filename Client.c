38e9fb5a368de1860e4213c21b06a51ab27b2f79
val17102 <32184574+val17102@users.noreply.github.com>
2020-04-15 17:40:27 -0600

    Yay

---
+++ Client.c
@@ -0,0 +1,63 @@
+#include<stdio.h> 
+#include<sys/types.h> 
+#include<sys/socket.h> 
+#include<sys/un.h> 
+#include<string.h> 
+#include<netdb.h> 
+#include<netinet/in.h> 
+#include<arpa/inet.h> 
+#include<stdlib.h> 
+  
+int main() 
+{ 
+    // Se crean buffers para comunicar el cliente y servidor, uno es para mensajes del cliente que envia
+	//y el otro es para los mensajes que vienen del servidor
+    char buffer1[256], buffer2[256]; 
+	//Se crean las estructuras para la direccion ip y puerto del cliente, y el servidor
+    struct sockaddr_in my_addr, my_addr1; 
+	//Se crea el socket del cliente
+    int client = socket(AF_INET, SOCK_STREAM, 0); 
+    if (client < 0) 
+    printf("Error in client creating\n"); 
+    else
+        printf("Client Created\n"); 
+    //Se define la direccion del servidor al que se quiere conectar el cliente
+    my_addr.sin_family = AF_INET; 
+	//Esto se pone por si no existe el ip al que se trata de conectar
+    my_addr.sin_addr.s_addr = INADDR_ANY; 
+	//Se define el puerto al que se quiere conectar
+    my_addr.sin_port = htons(8080); 
+      
+    // Este es el IP del servidor
+    my_addr.sin_addr.s_addr = inet_addr("192.168.100.4"); 
+  
+    // Se define la direccion del cliente que quiere conectarse
+    my_addr1.sin_family = AF_INET; 
+	//Esto se pone por si no existe el ip al que se trata de conectar
+    my_addr1.sin_addr.s_addr = INADDR_ANY; 
+	//Se define el puerto al que se quiere usar
+    my_addr1.sin_port = htons(8081); 
+      
+    // Este es el ip de la maquina del cliente
+    my_addr1.sin_addr.s_addr = inet_addr("192.168.100.5"); 
+	
+	//Se hace el bind del socket del cliente hacia el puerto e IP del servidor para poderse conectar
+    if (bind(client, (struct sockaddr*) &my_addr1, sizeof(struct sockaddr_in)) == 0) 
+        printf("Binded Correctly\n"); 
+    else
+        printf("Unable to bind\n"); 
+      
+    socklen_t addr_size = sizeof my_addr; 
+	//Se conecta al socket del servidor
+    int con = connect(client, (struct sockaddr*) &my_addr, sizeof my_addr); 
+    if (con == 0) 
+        printf("Client Connected\n"); 
+    else
+        printf("Error in Connection\n"); 
+    //Comunicacion
+    strcpy(buffer2, "Hello"); 
+    send(client, buffer2, 256, 0);  
+    recv(client, buffer1, 256, 0); 
+    printf("Server : %s\n", buffer1); 
+    return 0; 
+} 
---
+++ Server.c
@@ -0,0 +1,69 @@
+#include<stdio.h> 
+#include<sys/types.h> 
+#include<sys/socket.h> 
+#include<sys/un.h> 
+#include<string.h> 
+#include<netdb.h> 
+#include<netinet/in.h> 
+#include<arpa/inet.h> 
+#include<string.h> 
+  
+int main() 
+{ 
+    // Se crean buffers para comunicar el cliente y servidor, uno es para mensajes del servidor que envia
+	//y el otro es para los mensajes que vienen del cliente
+    char buffer1[256], buffer2[256]; 
+	// Se crea el socket
+    int server = socket(AF_INET, SOCK_STREAM, 0); 
+    if (server < 0) 
+        printf("Error in server creating\n"); 
+    else
+        printf("Server Created\n"); 
+    //Se crean las estructuras para la direccion ip y puerto del cliente, y el servidor
+    struct sockaddr_in my_addr, peer_addr; 
+    my_addr.sin_family = AF_INET; 
+	//Esto se pone por si no existe el ip al que se trata de conectar
+    my_addr.sin_addr.s_addr = INADDR_ANY; 
+      
+    // Este es el ip del servidor para que se puedan conectar los clientes
+    my_addr.sin_addr.s_addr = inet_addr("192.168.100.4"); 
+    //Puerto
+    my_addr.sin_port = htons(8080); 
+    //Se hace el bind del socket del servidor hacia el puerto e IP del servidor
+    if (bind(server, (struct sockaddr*) &my_addr, sizeof(my_addr)) == 0) 
+        printf("Binded Correctly\n"); 
+    else
+        printf("Unable to bind\n"); 
+    //El servidor se queda esperando a que alguien se conecte, con un limite de lista de espera de 3
+    if (listen(server, 3) == 0) 
+        printf("Listening ...\n"); 
+    else
+        printf("Unable to listen\n"); 
+      
+    socklen_t addr_size; 
+    addr_size = sizeof(struct sockaddr_in); 
+      
+    // Aqui se guarda el ip del cliente
+    char *ip; 
+      
+    // Se acepta conexiones 1 por 1  para siempre
+    while (1) 
+    { 
+        //Se acepta una coneccion
+        int acc = accept(server, (struct sockaddr*) &peer_addr, &addr_size); 
+        printf("Connection Established\n"); 
+        char ip[INET_ADDRSTRLEN]; 
+		//Obtiene info del cliente conectado como el ip 
+        inet_ntop(AF_INET, &(peer_addr.sin_addr), ip, INET_ADDRSTRLEN); 
+      
+        // ntohs sirve para obtener el puerto
+        printf("connection established with IP : %s and PORT : %d\n",  
+                                            ip, ntohs(peer_addr.sin_port)); 
+        //Comunicacion
+        recv(acc, buffer2, 256, 0); 
+        printf("Client : %s\n", buffer2); 
+        strcpy(buffer1, "Hello"); 
+        send(acc, buffer1, 256, 0); 
+    }  
+    return 0; 
+} 
