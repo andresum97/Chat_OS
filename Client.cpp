138b5d1bf43dda7225c369ea71973ac9e6dff94c
Andres Urizar <andresum1309@gmail.com>
2020-04-18 00:22:43 -0600

    sendInfo v1

--- Client.cpp
+++ Client.cpp
@@ -14,16 +14,19 @@ int main(int argc, char *argv[]){
     GOOGLE_PROTOBUF_VERIFY_VERSION;
     string username;
     string ip;
-    string port;
+    int port;
+    string buffer1, buffer2; 
+    struct sockaddr_in my_addr, my_addr1; 
+    int client = socket(AF_INET, SOCK_STREAM, 0); 
     if(argc==3){
         username = argv[0];
         ip = argv[1];
-        port = argv[2];
+        port = stoi(argv[2]);
     }else{
         printf("\n No ingreso todos los datos, debe ingresar (username,ip,port)");
     }
 
-    void sendInfo(string user, string ip, string port){
+    void sendInfo(string user, string ip){
         MyInfoSynchronize * clientInfo(new MyInfoSynchronize);
         clientInfo->set_username(user);
         clientInfo->set_ip(ip);
@@ -31,12 +34,15 @@ int main(int argc, char *argv[]){
         ClientMessage clientMessage;
         clientMessage.set_option(1);
         clientMessage.set_allocated_synchronize(clientInfo);
+
+        string msg;
+        clientMessage.SerializeToString(&msg);
+        send(client, msg, 256, 0);
     }
+    //============================================== CONEXION CON EL SERVIDOR ===============================================
     // Se crean buffers para comunicar el cliente y servidor, uno es para mensajes del cliente que envia
 	//y el otro es para los mensajes que vienen del servidor
-    char buffer1[256], buffer2[256]; 
-    struct sockaddr_in my_addr, my_addr1; 
-    int client = socket(AF_INET, SOCK_STREAM, 0); 
+    
     if (client < 0) 
     printf("Error in client creating\n"); 
     else
@@ -44,13 +50,13 @@ int main(int argc, char *argv[]){
 
     my_addr.sin_family = AF_INET; 
     my_addr.sin_addr.s_addr = INADDR_ANY; 
-    my_addr.sin_port = htons(8080); 
+    my_addr.sin_port = htons(port); 
 
-    my_addr.sin_addr.s_addr = inet_addr("192.168.100.4"); 
+    my_addr.sin_addr.s_addr = inet_addr(ip); 
 
     my_addr1.sin_family = AF_INET; 
     my_addr1.sin_addr.s_addr = INADDR_ANY; 
-    my_addr1.sin_port = htons(8081); 
+    my_addr1.sin_port = htons(port+1); 
 
     my_addr1.sin_addr.s_addr = inet_addr("192.168.100.5"); 
     if (bind(client, (struct sockaddr*) &my_addr1, sizeof(struct sockaddr_in)) == 0) 
@@ -64,6 +70,7 @@ int main(int argc, char *argv[]){
         printf("Client Connected\n"); 
     else
         printf("Error in Connection\n"); 
+    //====================================================================================================================
     char chr[257];
     while(strcmp(buffer1,"Exit")!=0){
 	recv(client, buffer1, 256, 0); 
