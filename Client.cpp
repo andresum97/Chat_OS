6e2e018d49756638515ccb0e6359369ee79bd289
gusta257 <gdeleontobias@gmail.com>
2020-04-18 11:46:15 -0600

    algunos arreglos

--- Client.cpp
+++ Client.cpp
@@ -7,26 +7,19 @@
 #include <netinet/in.h> 
 #include <arpa/inet.h> 
 #include <stdlib.h>
+#include <sstream>
+#include <iostream>
+#include <cstdlib>
+#include <pthread.h>
+#include <bits/stdc++.h>
+#include <arpa/inet.h>
+#include <chrono>
 #include "mensaje.pb.h"
-
 using namespace std;
-int main(int argc, char *argv[]){
-    GOOGLE_PROTOBUF_VERIFY_VERSION;
-    string username;
-    string ip;
-    int port;
-    string buffer1, buffer2; 
-    struct sockaddr_in my_addr, my_addr1; 
-    int client = socket(AF_INET, SOCK_STREAM, 0); 
-    if(argc==3){
-        username = argv[0];
-        ip = argv[1];
-        port = stoi(argv[2]);
-    }else{
-        printf("\n No ingreso todos los datos, debe ingresar (username,ip,port)");
-    }
+using namespace chat;
 
-    void sendInfo(string user, string ip){
+void sendInfo(char* user, string ip, int client){
+
         MyInfoSynchronize * clientInfo(new MyInfoSynchronize);
         clientInfo->set_username(user);
         clientInfo->set_ip(ip);
@@ -37,12 +30,50 @@ int main(int argc, char *argv[]){
 
         string msg;
         clientMessage.SerializeToString(&msg);
-        send(client, msg, 256, 0);
+	cout<< clientMessage.option()<<endl;
+        //send(client, msg, 256, 0);
     }
+
+int main(int argc, char *argv[]){
+	GOOGLE_PROTOBUF_VERIFY_VERSION;
+    
+    char* username;
+    char* ip;
+    char* port;
+    char buffer1[256], buffer2[256]; 
+    struct sockaddr_in my_addr, my_addr1; 
+    int client = socket(AF_INET, SOCK_STREAM, 0); 
+
+    if(argc>=3){
+        username = argv[1];
+        ip = argv[2];
+        port = argv[3];
+    }else{
+        printf("\n No ingreso todos los datos, debe ingresar (username,ip,port)");
+    }
+    printf("Username es %s\n",username);
+    printf("Ip es %s\n",ip);
+    printf("Port es %s\n",port);
     //============================================== CONEXION CON EL SERVIDOR ===============================================
     // Se crean buffers para comunicar el cliente y servidor, uno es para mensajes del cliente que envia
 	//y el otro es para los mensajes que vienen del servidor
-    
+ 
+    int puerto = atoi(port);
+	printf("Puerto es %d\n",puerto);
+	
+	
+    /*MyInfoSynchronize * clientInfo(new MyInfoSynchronize);
+    clientInfo->set_username(username);
+    clientInfo->set_ip(ip);
+
+    ClientMessage clientMessage;
+    clientMessage.set_option(1);
+    clientMessage.set_allocated_synchronize(clientInfo);
+
+    string msg;
+    clientMessage.SerializeToString(&msg);
+    send(client, msg, 256, 0);
+    */
     if (client < 0) 
     printf("Error in client creating\n"); 
     else
@@ -50,13 +81,13 @@ int main(int argc, char *argv[]){
 
     my_addr.sin_family = AF_INET; 
     my_addr.sin_addr.s_addr = INADDR_ANY; 
-    my_addr.sin_port = htons(port); 
+    my_addr.sin_port = htons(puerto); 
 
     my_addr.sin_addr.s_addr = inet_addr(ip); 
 
     my_addr1.sin_family = AF_INET; 
     my_addr1.sin_addr.s_addr = INADDR_ANY; 
-    my_addr1.sin_port = htons(port+1); 
+    my_addr1.sin_port = htons(puerto+1); 
 
     my_addr1.sin_addr.s_addr = inet_addr("192.168.100.5"); 
     if (bind(client, (struct sockaddr*) &my_addr1, sizeof(struct sockaddr_in)) == 0) 
@@ -82,5 +113,6 @@ int main(int argc, char *argv[]){
 	printf("Server : %s\n", buffer1);
 	scanf("%c",&chr);
     }
+	google::protobuf::ShutdownProtobufLibrary();
     return 0; 
 }
