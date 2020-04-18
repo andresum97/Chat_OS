fe9cb76e53b402acee0d54bed4dd3876b1974044
Andres Urizar <andresum1309@gmail.com>
2020-04-17 22:07:10 -0600

    client cpp

---
+++ Client.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <sys/un.h> 
+#include <string.h> 
+#include <netdb.h> 
+#include <netinet/in.h> 
+#include <arpa/inet.h> 
+#include <stdlib.h>
+#include "mensaje.pb.h"
+
+int main(){
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+    
+
+}
